209d9b50374f8cea6db06ae3babd1661141a080b
JasonPeng <guo_tai@163.com>
2019-06-04 10:41:38 +0800

    init

---
+++ ch4-3-httpclient/src/main/java/com/springcloud/jp/feign/SpringCloudFeignApplication.java
@@ -0,0 +1,13 @@
+package com.springcloud.jp.feign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients
+public class SpringCloudFeignApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudFeignApplication .class,args);
+    }
+}
